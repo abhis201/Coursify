ce1bee2a2d1ad9307f4baecebe069826a69bf3e7
singhAbhiPNW <sing1290@pnw.edu>
2023-12-18 15:11:52 -0600

    server url fix

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'https://coursify-server-g4vr.onrender.com/';
+export const BASE_URL = 'https://coursify-server-g4vr.onrender.com';
